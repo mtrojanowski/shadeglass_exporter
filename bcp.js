69adf6afa4b5fab5482984543f6c7929ea7706ea
Michal Trojanowski <michal@curity.io>
2025-07-01 11:55:06 +0200

    Handle missing warbands in BCP and give more information about players with missing data

--- bcp.js
+++ bcp.js
@@ -8,7 +8,12 @@ const eventId = ''
 const token = ''
 
 /**
- * "<userId>": { "deck1": "", "deck2": "" }
+ * warband is optional, you can set it if there is no warband in BCP for the given player.
+ * Use the warband name from the stats as the value.
+ * If you do set the warband, also set the deck values, so avoid entries like this: "abcd": { warband: "Crimson Court" }
+ *
+ * Use the following format for entries:
+ * "<userId>": { "deck1": "", "deck2": "", warband: "" }
  */
 const parsedDecklists = {
 }
@@ -181,16 +186,36 @@ function parseDecks(deckUrl) {
     return decks;
 }
 
-async function getDecks(playerId, decklistId) {
+function getPlayerWarband(playerInfo) {
+    const warband = playerInfo['armyName'];
+
+    if (!warband) {
+        console.log(`No warband information for user ID ${playerInfo['userId']}, ${playerInfo['firstName']} ${playerInfo['lastName']}`)
+        return '-'
+    }
+
+    return bcpFactionMap[warband];
+}
+
+async function getDecks(playerInfo) {
+    const playerId = playerInfo['userId']
     const parsed = parsedDecklists[playerId]
     if (parsed) {
+        if (!parsed['warband']) {
+            const warband = getPlayerWarband(playerInfo)
+            parsed['warband'] = warband
+            parsedDecklists[playerId]['warband'] = warband
+        }
         return parsed
     }
 
-    const emptyDeckList = {deck1: "-", deck2: "-", deckUrl: ""}
+    const warband = getPlayerWarband(playerInfo)
+    const emptyDeckList = {deck1: "-", deck2: "-", deckUrl: "", warband }
+
+    const decklistId = playerInfo['armyListObjectId'];
 
     if (decklistId === undefined) {
-        console.log('No decklist ID for player ' + playerId);
+        console.log(`No decklist ID for player with ID ${playerId}, ${playerInfo['firstName']} ${playerInfo['lastName']}`);
         parsedDecklists[playerId] = emptyDeckList
         return emptyDeckList
     }
@@ -200,7 +225,7 @@ async function getDecks(playerId, decklistId) {
     const decklistText = bcpDecklist["armyListText"]
 
     if (decklistText === undefined) {
-        console.log('No decklist text for player ' + playerId)
+        console.log(`No decklist text for player with ID ${playerId}, ${playerInfo['firstName']} ${playerInfo['lastName']}`)
         console.log('Returned deck data for the player: ' + JSON.stringify(bcpDecklist))
         parsedDecklists[playerId] = emptyDeckList
         return emptyDeckList
@@ -211,14 +236,14 @@ async function getDecks(playerId, decklistId) {
         const deckUrl = urlMatch ? urlMatch[0] : '';
 
         const decks = parseDecks(decklistText)
-        const parsed = { deck1: deckMap[decks[0]], deck2: deckMap[decks[1]] }
+        const parsed = { deck1: deckMap[decks[0]], deck2: deckMap[decks[1]], warband }
         parsed['deckUrl'] = deckUrl
 
         parsedDecklists[playerId] = parsed
         return parsed
     }
 
-    console.log('Cannot find underworldsdb link in the player`s deck list data. Trying to find information from the text, for player: ' + playerId)
+    console.log(`Cannot find underworldsdb link in the player's deck list data. Trying to find information from the text, for player with ID ${playerId}, ${playerInfo['firstName']} ${playerInfo['lastName']}`)
 
     // Try to get the decks from the text itself
     const decks = []
@@ -229,14 +254,14 @@ async function getDecks(playerId, decklistId) {
     }
 
     if (decks.length !== 2) {
-        console.log(`Error: Could not find exactly two decks in the decklist text for player ${playerId}. Found: ${decks.length} decks. Text: ${decklistText}`);
+        console.log(`Could not find exactly two decks in the decklist text for player ${playerId}, ${playerInfo['firstName']} ${playerInfo['lastName']}. Found: ${decks.length} decks. Text: ${decklistText}`);
         parsedDecklists[playerId] = emptyDeckList
         return emptyDeckList
     }
 
-    console.log('Found decks in the text for player ' + playerId + '. The original returned deck data: ' + JSON.stringify(bcpDecklist));
+    console.log(`Found decks in the text for player with ID ${playerId}, ${playerInfo['firstName']} ${playerInfo['lastName']}. The original returned deck data: ${JSON.stringify(bcpDecklist)}`);
 
-    const parsedDeck = { deck1: decks[0], deck2: decks[1] }
+    const parsedDeck = { deck1: decks[0], deck2: decks[1], warband }
     console.log(`Use the following entry if you want to add this player to the parsedDecklists: '${playerId}': ${JSON.stringify(parsedDeck)},`);
 
     parsedDecklists[playerId] = parsedDeck
@@ -244,16 +269,17 @@ async function getDecks(playerId, decklistId) {
 }
 
 async function processSingleGameResult(gameResult) {
-    if (gameResult['metaData'] && gameResult['player2']['armyName'] && gameResult['player1']['armyName']) {
-        const player1Decks = await getDecks(gameResult['player1']['userId'], gameResult['player1']['armyListObjectId'])
-        const player2Decks = await getDecks(gameResult['player2']['userId'], gameResult['player2']['armyListObjectId'])
+    // Process only pairings where both userIDs are present. When one of the user ID's is missing, then it was a bye, and we skip that.
+    if (gameResult['metaData'] && gameResult['player2']['userId'] && gameResult['player1']['userId']) {
+        const player1Decks = await getDecks(gameResult['player1'])
+        const player2Decks = await getDecks(gameResult['player2'])
         return {
-            player1Faction: bcpFactionMap[gameResult['player1']['armyName']],
+            player1Faction: player1Decks['warband'],
             player1Deck1: player1Decks['deck1'],
             player1Deck2: player1Decks['deck2'],
             gamesWon: gameResult['metaData']['in-p1-numberOfWins'] || 0,
             gamesLost: gameResult['metaData']['in-p2-numberOfWins'] || 0,
-            player2Faction: bcpFactionMap[gameResult['player2']['armyName']],
+            player2Faction: player2Decks['warband'],
             player2Deck1: player2Decks['deck1'],
             player2Deck2: player2Decks['deck2']
         }
@@ -296,7 +322,8 @@ async function printPlacings() {
 
         const playerId = placing['userId']
         const playerFaction = bcpFactionMap[placing['faction']['name']]
-        const playerDecks = await getDecks(playerId, placing['listId'])
+        // TODO fix this!
+        // const playerDecks = await getDecks(playerId, placing['listId'])
 
         playersAndDecks.push([placing['placing'], playerFaction, playerDecks['deck1'], playerDecks['deck2'], playerDecks['deckUrl']])
     }
