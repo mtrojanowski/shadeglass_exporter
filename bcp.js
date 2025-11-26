79893f4e9046f97d4722d8f571303fe7eed19c06
Michal Trojanowski <michal@curity.io>
2025-11-26 22:39:37 +0100

    New decks and warbands in the legacy script

--- bcp.js
+++ bcp.js
@@ -37,7 +37,9 @@ const deckMap = {
     'EK': 'Edge of the Knife',
     'RF': 'Reckless Fury',
     'RS': 'Realmstone Raiders',
-    'RG': 'Raging Slayers'
+    'RG': 'Raging Slayers',
+    'DY': 'Deadly Synergy',
+    'HG': 'Hunting Grounds'
 }
 
 const bcpFactionMap = {
@@ -110,7 +112,9 @@ const bcpFactionMap = {
     "Jaws of Itzl": 'Jaws of Itzl',
     "Borgit's Beastgrabbaz": "Borgit's Beastgrabbaz",
     'Knives of the Crone': 'Knives of the Crone',
-    "Kamandora's Blades": "Kamandora's Blades"
+    "Kamandora's Blades": "Kamandora's Blades",
+    "Kurnoth's Heralds": "Kurnoth's Heralds",
+    "Blood of the Bull": "Blood of the Bull"
 }
 
 
